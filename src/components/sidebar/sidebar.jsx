7c5014d83b3d7bc9d10ab2ee5de84d0428491501
mirjalol03 <jabborovmirjalol03@gmail.com>
2023-04-25 04:03:02 +0500

    big change, style and logic changed

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import "./sidebar.scss";
-import {
-  MdSubscriptions,
-  MdExitToApp,
-  MdThumbUpAlt,
-  MdHistory,
-  MdLibraryBooks,
-  MdHome,
-  MdSentimentDissatisfied,
-} from "react-icons/md";
+import { MdExitToApp } from "react-icons/md";
+
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
+
 import { useDispatch } from "react-redux";
 import { log_out } from "../../redux/actions/auth.action";
 import { Link } from "react-router-dom";
+import { navbarLinks } from "../../utils/navbar";
 const Sidebar = ({ sidebar, handleToggleSidebar }) => {
+  const [active, setActive] = React.useState(1);
+
   const dispatch = useDispatch();
   const logoutHandler = () => {
     dispatch(log_out());
+  };
+
+  const handleChangeActive = (itemLink, itemId) => {
+    if (itemLink === "/") {
+      setActive(1);
+    } else {
+      setActive(itemId);
+    }
   };
 
   return (
@@ -24,38 +30,51 @@ const Sidebar = ({ sidebar, handleToggleSidebar }) => {
       onClick={() => handleToggleSidebar(false)}
     >
       <ul className="sidebar-links">
-        <Link to={'/' }>
+        {navbarLinks.main.length > 0 &&
+          navbarLinks.main.map((item) => (
+            <Link
+              onClick={() => handleChangeActive(item.link, item.id)}
+              to={item.link}
+              className={`item-link ${item.id === active ? "active" : ""}`}
+              key={item.id}
+            >
+              <li className="side-menu__items__item">
+                <item.icon></item.icon>
 
-        <li>
-          <MdHome size={23} /> <span>Home</span>{" "}
-        </li>
-        </Link>
-        <li>
-          <MdSubscriptions size={23} /> <span>Subscription</span>{" "}
-        </li>
-        <li>
-          <MdThumbUpAlt size={23} /> <span>LikedVideos</span>{" "}
-        </li>
-        <li>
-          <MdHistory size={23} /> <span>History</span>{" "}
-        </li>
-
-        <li>
-          <MdLibraryBooks size={23} />
-          <span>Library</span>
-        </li>
-        <li>
-          <MdSentimentDissatisfied size={23} />
-          <span>I don`t know</span>
-        </li>
+                <span>{item.text}</span>
+              </li>
+            </Link>
+          ))}
 
         <hr />
+
+        {navbarLinks.secondary.length > 0 &&
+          navbarLinks.secondary.map((item) => (
+            <Link
+              onClick={() => handleChangeActive(item.link, item.id)}
+              to={item.link}
+              className={`item-link secondary-item ${item.id === active ? "active" : ""}`}
+              key={item.id}
+            >
+              <li className="side-menu__items__item">
+                <item.icon></item.icon>
+
+                <span>{item.text}</span>
+              </li>
+            </Link>
+          ))}
+        <li className="secondary-item">
+          <ExpandMoreOutlinedIcon />
+          <span>Show more</span>
+        </li>
+
+        <hr className="secondary-item"/>
 
         <li onClick={logoutHandler}>
           <MdExitToApp size={23} />
           <span>Log out</span>
         </li>
-        <hr />
+        {/* <hr /> */}
       </ul>
     </nav>
   );
